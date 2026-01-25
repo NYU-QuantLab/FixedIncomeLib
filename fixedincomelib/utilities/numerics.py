Fix interpolator gradient again!!! and enable model jacobian calculation

fixedincomelib/utilities/numerics.py
@@ -184,7 +184,7 @@ class Interpolator1DPCP(Interpolator1D):
             elif i == self.length:
                 interval_s, interval_e, interval_i = self.axis1[-1], np.inf, self.length - 1                 
             else:
-                interval_s, interval_e, interval_i = self.axis1[i-1], self.axis1[i], 0
+                interval_s, interval_e, interval_i = self.axis1[i-1], self.axis1[i], i
             # if both of them are in the same interval
             if start_x >= interval_s and start_x < interval_e and \
                 end_x >= interval_s and end_x < interval_e:

fixedincomelib/valuation/utilities.py
@@ -1,4 +1,5 @@
 from typing import Any
+import numpy as np
 from fixedincomelib.model import *
 from fixedincomelib.product import *
 from fixedincomelib.valuation.report import PVCashReport
@@ -34,6 +35,16 @@ def create_value_report(
 def risk_calculation(engine : ValuationEngineProduct):
     gradient = []
     engine.calculate_first_order_risk(gradient, 1., False)
-    # TODO: chain model jacobian
-    return gradient # <- this will change
+    # dV/dX^M = dV/dX^I \cdot J^{-1}, where J is the model jacobian dX^M/dX^I 
+    # step 1: flatten gradient
+    gradient_flat = np.concatenate(gradient, axis=0) # dim : 1 x len(X^I)
+    # step 2: get model (lazy) jacobian
+    jacobian = engine.model.calculate_model_jacobian()
+    # step 3: inverse model jacobian (what is this algorithm ?)
+    jacobian_inv = np.linalg.inv(jacobian)
+    # step 4: final risk
+    risk = np.dot(gradient_flat, jacobian_inv)
+    # step 5: make a nice report
+    
+    return risk
 

fixedincomelib/yield_curve/valuation_engine.py
@@ -125,7 +125,6 @@ class ValuationEngineProductRfrFuture(ValuationEngineProduct):
             CompoundingMethod.COMPOUND
         )
 
-        self.df_ = 1.0
         self.forward_rate_ = 0.0
     
     @classmethod
@@ -135,25 +134,16 @@ class ValuationEngineProductRfrFuture(ValuationEngineProduct):
     def calculate_value(self):
 
         self.dvdfwd_ = 0.
-        self.dvddf_ = 0.
-        self.payoff_ = 0.
 
         if self.value_date <= self.effective_date_:
             self.index_engine_.calculate_value()
             self.forward_rate_ = self.index_engine_.value()
             
-            self.payoff_ = self.sign_ * self.notional_ * (100. - 100. * self.forward_rate_ - self.strike_)
-
-            if self.value_date == self.effective_date_:
-                self.value_ = self.cash_ = self.payoff_
-            else:
-                funding_model: YieldCurve = self.model_
-                self.df_ = funding_model.discount_factor(self.funding_index_, self.effective_date_)
-                self.value_ = self.payoff_ * self.df_
-                # risk
-                self.dvdfwd_ = -100. * self.sign_ * self.notional_ * self.df_
-                self.dvddf_ = self.value_ / self.payoff_
-
+            # due to mtm, future does not require discounting
+            self.value_ = self.sign_ * self.notional_ * (100. - 100. * self.forward_rate_ - self.strike_)
+            # risk
+            if self.value_date != self.effective_date_:
+                self.dvdfwd_ = -100. * self.sign_ * self.notional_
     
     def calculate_first_order_risk(self, gradient=None, scaler: float = 1.0, accumulate: bool = False) -> None:
 
@@ -162,14 +152,6 @@ class ValuationEngineProductRfrFuture(ValuationEngineProduct):
 
         if self.value_date < self.effective_date_:
             self.index_engine_.calculate_risk(local_grad, scaler * self.dvdfwd_, True)
-            funding_model: YieldCurve = self.model_
-            funding_model.discount_factor_gradient_wrt_state(
-                self.funding_index_,
-                self.effective_date_,
-                local_grad,
-                scaler * self.dvddf_,
-                True
-            )
 
         self.model_.resize_gradient(gradient)
         if accumulate:
@@ -188,9 +170,9 @@ class ValuationEngineProductRfrFuture(ValuationEngineProduct):
             self.notional_,
             self.sign_,
             self.effective_date_,
-            self.payoff_,
             self.value_,
-            self.df_,
+            self.value_,
+            1.,
             fixing_date=self.termination_date_,
             start_date=self.effective_date_,
             end_date=self.termination_date_,
@@ -209,7 +191,12 @@ class ValuationEngineProductRfrFuture(ValuationEngineProduct):
     def par_rate_or_spread(self) -> float:
         return self.forward_rate_
 
-
+    def grad_at_par(self) -> np.ndarray: 
+        local_grad = []
+        self.model_.resize_gradient(local_grad)
+        if self.value_date < self.effective_date_:
+            self.index_engine_.calculate_risk(local_grad, -100., True)
+        return local_grad
 
 ### register
 ValuationEngineProductRegistry().register((YieldCurve._model_type.to_string(), 

fixedincomelib/yield_curve/yield_curve_model.py
@@ -1,14 +1,16 @@
 import numpy as np
 from typing import List
+from scipy.linalg import block_diag
 
-from yaml import serialize
 from fixedincomelib.date import *
 from fixedincomelib.data import *
 from fixedincomelib.market import *
 from fixedincomelib.model import *
 from fixedincomelib.product import *
 from fixedincomelib.utilities import *
+from fixedincomelib.valuation import *
 from fixedincomelib.yield_curve.build_method import YieldCurveBuildMethod
+from fixedincomelib.valuation.valuation_engine import ValuationRequest
 
 class YieldCurve(Model):
 
@@ -63,6 +65,36 @@ class YieldCurve(Model):
         func = ModelBuilderRegistry().get(model_type)
         return func(value_date, dc, bmc)
 
+    def calculate_model_jacobian(self):
+        super().calculate_model_jacobian()
+        # WARNING: WE DO NOT ALLOW A MIXTURE OF CALIBRATION INSTRUMENTS AND STATE DATA FOR NOW
+        only_state_data = False
+        jacobian_pre = [None] * self.num_components
+        for target_name, yc_component in self.components_.items():
+            index = self.component_indices[target_name]
+            calib_prod = yc_component.calibration_product
+            calib_funding = yc_component.calibration_funding
+            if len(calib_prod) == 0:
+                # no calibration, just using state data
+                # jacobian is identity
+                jacobian_pre[index] = np.diag(np.ones(yc_component.num_state_data))
+                only_state_data = True
+                continue
+            # calculate calibration instrument gradient
+            grads = []
+            for _, (prod, funding) in enumerate(zip(calib_prod, calib_funding)):
+                fi_vp = FundingIndexParameter({'Funding Index' : funding})
+                vpc = ValuationParametersCollection([fi_vp])
+                engine = ValuationEngineProductRegistry.new_valuation_engine(
+                    self, prod, vpc, ValuationRequest.PV_DETAILED)
+                engine.calculate_value()
+                grads.append(engine.grad_at_par())
+            jacobian_pre[index] = np.concatenate(grads, axis=0)
+
+        jacobian = jacobian = block_diag(*jacobian_pre) if only_state_data else np.concatenate(jacobian_pre, axis=0)
+        self.is_jacobian_calculated_ = True
+        return jacobian
+        
 class YieldCurveModelComponent(ModelComponent):
 
     def __init__(self, 
@@ -122,5 +154,9 @@ class YieldCurveModelComponent(ModelComponent):
     def state_data_interpolator(self) -> Interpolator1D:
         return self.interpolator_
     
+    @property
+    def num_state_data(self) -> int:
+        return self.num_state_data_
+
 ### registry
 ModelDeserializerRegistry().register(YieldCurve._model_type.to_string(), YieldCurve.deserialize)